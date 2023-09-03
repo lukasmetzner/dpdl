30f9f23067e64f8b40df63f7a9dbcad91e6bdd19
lukasmetzner <lukas@metzner.io>
2023-09-03 19:02:54 +0200

    recursive conversion to pipeline instructions

--- src/lib.rs
+++ src/lib.rs
@@ -8,22 +8,43 @@ pub enum Instruction<'a, 'b> {
     Input(Node<'a, 'b>),
     Step(Node<'a, 'b>),
     File(Node<'a, 'b>),
+    Print(Node<'a, 'b>),
 }
 
 impl Instruction<'_, '_> {
     pub fn execute(&self) {
         match self {
-            Instruction::Input(node) => println!("{:?}", node),
-            Instruction::Step(node) => println!("{:?}", node),
-            Instruction::File(node) => println!("{:?}", node),
+            Instruction::Input(node) => self._input_ins(node),
+            Instruction::Step(node) => self._step_ins(node),
+            Instruction::File(node) => self._file_ins(node),
+            Instruction::Print(node) => self._print_ins(node),
         }
     }
+
+    fn _input_ins(&self, node: &Node<'_, '_>) { println!("{:?}", node); }
+    fn _step_ins(&self, node: &Node<'_, '_>) { println!("{:?}", node); }
+    fn _file_ins(&self, node: &Node<'_, '_>) { println!("{:?}", node); }
+    fn _print_ins(&self, node: &Node<'_, '_>) { println!("{:?}", node); }
 }
 
-// pub fn parse_desendants(node: &Node) -> Vec<Instruction> {
-//     let instructions: Vec<Instruction> = Vec::new();
-//     instructions
-// }
+pub fn parse_descendants<'a, 'b>(node: &Node<'a, 'b>) -> Vec<Instruction<'a, 'b>> {
+    let mut instructions: Vec<Instruction<'a, 'b>> = Vec::new();
+    for child in node.children() {
+        if child.is_text() {
+            continue;
+        }
+        match child.tag_name().name() {
+            "input" => instructions.push(Instruction::Input(child)),
+            "step" => instructions.push(Instruction::Step(child)),
+            "file" => instructions.push(Instruction::File(child)),
+            "print" => instructions.push(Instruction::Print(child)),
+            _ => panic!("unknown xml tag"),
+        }
+        let sub_instructions = parse_descendants(&child);
+        instructions.extend(sub_instructions);
+    }
+    instructions
+}
 
 pub fn execute(path: &Path) {
     let text = fs::read_to_string(path).unwrap();
@@ -31,18 +52,6 @@ pub fn execute(path: &Path) {
 
     let root_element = doc.root_element();
 
-    let mut instructions: Vec<Instruction> = Vec::new();
-    for descendant in root_element.children() {
-        if descendant.is_text() { continue };
-        match descendant.tag_name().name() {
-            "input" => instructions.push(Instruction::Input(descendant)),
-            "step" => instructions.push(Instruction::Step(descendant)),
-            "file" => instructions.push(Instruction::Step(descendant)),
-            _ => panic!("unkown xml tag"),
-        }
-    }
-    
-    for instruction in instructions {
-        instruction.execute();
-    }
+    let instructions: Vec<Instruction> = parse_descendants(&root_element);
+    instructions.into_iter().for_each(|f| f.execute());
 }
